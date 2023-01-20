c60ba012c3b2bb5a75a1ba7d8e19f584581e4069
Mekdie <mcdonyleee@gmail.com>
2023-01-20 11:43:36 +1100

    Added mock data for frontend using react and dsplay basic

---
+++ src/data/MockData.js
@@ -0,0 +1,17 @@
+export const  books = [
+    {
+        id: "9b0896fa-3880-4c2e-bfd6-925c87f22878",
+        name: "CQRS for Dummies",
+        reserved: false,
+    },
+    {
+        id: "0550818d-36ad-4a8d-9c3a-a715bf15de76",
+        name: "Visual Studio Tips",
+        reserved: false,
+    },
+    {
+        id: "8e0f11f1-be5c-4dbc-8012-c19ce8cbe8e1",
+        name: "NHibernate Cookbook",
+        reserved: false
+    }
+]
