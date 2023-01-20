7f912f633a861f60179562508c2973f2533034c1
Mekdie <mcdonyleee@gmail.com>
2023-01-20 12:21:44 +1100

    Made a table for better list all books

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import "./App.css";
 import { books } from "./data/MockData";
 import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 function App() {
   const [booksList, setBooksList] = useState(books);
   return (
     <div className="App">
-      {booksList.map((book) => {
-        return (
-          <>
-            <h1>{book.name}</h1>
-            <h3>{book.id}</h3>
-          </>
-        );
-      })}
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Reserved?</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {booksList.map((book, index) => {
+            return (
+              <>
+                <tr key={book.id}>
+                  <td>{index + 1}</td>
+                  <td>{book.name}</td>
+                  <td>{book.reserved ? "Yes" : "No"}</td>
+                  <td>
+                    <Button variant="primary">Reserve</Button>{" "}
+                    <Button variant="danger">Cancel</Button>
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 }
