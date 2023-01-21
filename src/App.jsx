12c4283e77f8c76a6c2c3dccb446962769cc4d9f
McDony Lee <mcdonyleee@gmail.com>
2023-01-21 19:17:33 +1100

    Added cancel book reservation

--- src/App.jsx
+++ src/App.jsx
@@ -8,83 +8,109 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function App() {
-  const [booksList, setBooksList] = useState(books);
-  const [search, setSearch] = useState("");
+    const [booksList, setBooksList] = useState(books);
+    const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    console.log(search);
-    let searchFilter = books.filter((book) =>
-      book.name.toLowerCase().includes(search.toLowerCase())
+    useEffect(() => {
+        let searchFilter = books.filter((book) =>
+            book.name.toLowerCase().includes(search.toLowerCase())
+        );
+        setBooksList(searchFilter);
+    }, [search]);
+
+    const reserveBook = (id) => {
+        //reserve book here and changed the reserved value to true
+        const bookingId = crypto.randomUUID();
+        alert(`Booking ID: ${bookingId}`);
+        const index = booksList.findIndex((book) => book.id === id);
+
+        const copy = [...booksList]; //make a copy
+        copy[index] = {
+            ...books[index],
+            ...{ reserved: true, bookingId: bookingId },
+        }; //update the value using es6 spread operator
+        setBooksList(copy);
+    };
+
+    const cancelBook = (bookingId) => {
+        const index = booksList.findIndex(
+            (book) => book.bookingId === bookingId
+        );
+
+        const copy = [...booksList]; //make a copy
+        copy[index] = {
+            ...books[index],
+            ...{ reserved: false },
+        }; //update the value using es6 spread operator
+        setBooksList(copy);
+    };
+
+    return (
+        <div className="App">
+            <Container className="no-padding">
+                <Row>
+                    <Col>
+                        <input
+                            onChange={(e) => setSearch(e.target.value)}
+                            value={search}
+                            className="form-control mr-sm-4"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+            <br />
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Reserved?</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {booksList &&
+                        booksList.map((book, index) => {
+                            return (
+                                <tr key={book.id}>
+                                    <td>{index + 1}</td>
+                                    <td>{book.name}</td>
+                                    <td>{book.reserved ? "Yes" : "No"}</td>
+                                    <td>
+                                        <Button
+                                            onClick={() => reserveBook(book.id)}
+                                            variant={
+                                                book.reserved
+                                                    ? "secondary"
+                                                    : "primary"
+                                            }
+                                            disabled={
+                                                book.reserved ? true : false
+                                            }
+                                        >
+                                            {book.reserved
+                                                ? "Reserved"
+                                                : "Reserve"}
+                                        </Button>{" "}
+                                        <Button
+                                            onClick={() =>
+                                                cancelBook(book.bookingId)
+                                            }
+                                            variant="danger"
+                                        >
+                                            Cancel
+                                        </Button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </Table>
+            {booksList.length === 0 && <h4>No books found</h4>}
+        </div>
     );
-    setBooksList(searchFilter);
-  }, [search]);
-
-  const reserveBook = (id) => {
-    //reserve book here and changed the reserved value to true
-    const bookingId = crypto.randomUUID();
-    alert(`Book ${id} reserved. Booking ID: ${bookingId}`);
-    const index = booksList.findIndex((book) => book.id === id);
-
-    const copy = [...booksList]; //make a copy
-    copy[index] = {
-      ...books[index],
-      ...{ reserved: true, bookingId: bookingId },
-    }; //update the value using es6 spread operator
-    console.log(copy);
-    setBooksList(copy);
-  };
-
-  return (
-    <div className="App">
-      <Container className="no-padding">
-        <Row>
-          <Col>
-            <input
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-              className="form-control mr-sm-4"
-              placeholder="Search"
-              aria-label="Search"
-            />
-          </Col>
-        </Row>
-      </Container>
-      <br />
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Reserved?</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {booksList.map((book, index) => {
-            return (
-              <>
-                <tr key={book.id}>
-                  <td>{index + 1}</td>
-                  <td>{book.name}</td>
-                  <td>{book.reserved ? "Yes" : "No"}</td>
-                  <td>
-                    <Button
-                      onClick={() => reserveBook(book.id)}
-                      variant={book.reserved ? "secondary" : "primary"}
-                      disabled={book.reserved ? true : false}
-                    >
-                      {book.reserved ? "Reserved" : "Reserve"}
-                    </Button>{" "}
-                    <Button variant="danger">Cancel</Button>
-                  </td>
-                </tr>
-              </>
-            );
-          })}
-        </tbody>
-      </Table>
-    </div>
-  );
 }
 
 export default App;
